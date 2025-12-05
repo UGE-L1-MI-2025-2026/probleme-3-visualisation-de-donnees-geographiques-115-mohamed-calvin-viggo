be7567830757fe8659ff893b2ccaeca221f4ed00
Mohamed Tahar <mohamed.tahar@edu.univ-eiffel.fr>
2025-12-05 12:40:02 +0100

    nouvvelle constantes pour fenetre

--- constantes.py
+++ constantes.py
@@ -1,10 +1,10 @@
 # dimensions de la fenetre
 Largeur = 800
 Hauteur = 750
-LARGEUR_FENETRE = 1000
+LARGEUR_FENETRE = 900
 HAUTEUR_FENETRE = 850
-Y_DEBUT = 20
-X_DEBUT = 20
+Y_DEBUT = -10
+X_DEBUT = 0
 # coordonnees geographiques de la france
 MIN_LON = -5.5
 MAX_LON = 10.0
